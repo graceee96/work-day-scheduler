370aadb4b16291a424b7fc44f4c34f85d4b0d1af
Grace Roh <grace@BIGMINE.attlocal.net>
2023-03-17 23:41:36 -0700

    write README

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # Work Day Scheduler
 
+## Description
+This work day scheduler was made to assist users with managing their time much more effectively. The color changing functionality of the application allows user to keep track of their tasks and see if tasks are completed on time. 
 
-https://www.youtube.com/watch?v=dfahGt2YT64&ab_channel=ChillyFilly
+## Installation
+N/A
+
+## Usage
+To use this application, input tasks that must be completed in the appropriate time slot. Time slots change colors in correspondance to the time. Time slots for the current hour are red, while past time slots are grey and future time slots are green.
+
+In addition, clicking on the blue save button allows users to store the tasks. These tasks will stay on the page even if the webpage closes or refreshes.
+
+Website URL:
+
+## Credit
+
+## License
+Please refer to LICENSE in the repository
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
 
+//html elements
+var saveButton = $('.saveBtn');
+
 //html elements -- divs
 var hourNineDiv = $('#hour-9');
 var hourTenDiv = $('#hour-10');
@@ -27,24 +30,21 @@ var divElements = [
 //variables
 var timeNow = dayjs();
 var currentHour = timeNow.format('H'); //this returns a string***
-
 console.log(currentHour)
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage.
+  //HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
+  // saveButton.on('click', function() {
+  //   console.log(this);
+  //   var schedule = {
+  //     hourNineDiv =
+  //   }
+  // })
+  // console.log(this);
 
 
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
       // make an array of element and hours {div element, hour} --> set value of each element of to the hour
       // use setInterval() --> declare new variable currentHour and check if currentHour is equal to/greater than/less than the value of each element;
       // use if else to change classes of elements
@@ -72,9 +72,7 @@ $(function () {
     }
   }, 1000)
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
 
 
   // TODO: Add code to display the current date in the header of the page.
